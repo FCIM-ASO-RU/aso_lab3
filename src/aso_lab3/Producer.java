5f25a46cd57dc95060225a85bff2a7c83bb9b5c2
Valelaaa <kosneanuvaleriy@gmail.com>
2023-04-06 21:43:05 +0300

    Producer.java Implementation

--- src/aso_lab3/Producer.java
+++ src/aso_lab3/Producer.java
@@ -2,23 +2,54 @@ package aso_lab3;
 
 import java.util.concurrent.CyclicBarrier;
 
-public class Producer extends Thread {
-    
-    private static final int max = 0; // указываете в соответствии с вариантом
-    private static final ReplaceMe[] products = new ReplaceMe[]{}; // меняете на то, что должно производится
-    
-    private final Store store;
-    private final CyclicBarrier barrier;
-    
-    public Producer(Store _store, CyclicBarrier _barrier, String name) {
-        store = _store;
-        barrier = _barrier;
-        setName(name);
+class Producer extends Thread {
+    private final CyclicBarrier barr;
+    private final Store data;
+    private final String name;
+    private final String[] vawel;
+    static int max = 0;
+
+    public Producer(final Store data, final CyclicBarrier barr, final String name) {
+        this.data = data;
+        this.name = name;
+        this.barr = barr;
+        vawel = new String[]{"Lipsi HA", "Za деньги да", "KAK MOMMY", "POPSTAR", "MONEYKEN LOVE"};
     }
-    
+
     @Override
     public void run() {
-        
+        int tmp;
+        boolean response;
+        int prod = 0;
+
+        while (true) {
+            try {
+                if (max < 54) {
+                    tmp = (int) (Math.random() * 5);
+                    response = data.store(vawel[tmp], name);
+
+                    if (response) {
+                        barr.await();
+                    } else {
+                        System.out.println(name + " produced " + vawel[tmp]);
+                        prod++;
+                        max++;
+                        sleep((int) (Math.random() * 1000));
+                    }
+                } else {
+                    tmp = (int) (Math.random() * 5);
+                    response = data.store(vawel[tmp], name);
+                    if (response) {
+                        barr.await();
+                    }
+
+                    sleep(2000);
+                    System.out.println(name + " produced " + prod + " elements");
+                    break;
+                }
+            } catch (final Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
