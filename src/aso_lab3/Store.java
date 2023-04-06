f3b8e418329fba6ab49054df45a6d16394da8848
whereareyoo <ramin-gashimov@mail.ru>
2023-04-06 21:50:41 +0300

    store implementation

--- src/aso_lab3/Store.java
+++ src/aso_lab3/Store.java
@@ -1,27 +1,58 @@
 package aso_lab3;
 
+import java.util.LinkedList;
 import java.util.concurrent.Semaphore;
 
-public class Store {
-    
-    private static final int capacity = 0; // указываете в соответствии с вариантом
-    private boolean available; 
-    private final ReplaceMe stock; // меняете ReplaceMe на желаемый вами класс-коллекцию
-    
-    final Semaphore producers = new Semaphore(1, true);
-    final Semaphore consumers = new Semaphore(1, true);
-    
-    public Store() {
-        available = false;
-        stock = new ReplaceMe();
+class Store {
+    private final LinkedList<String> stock = new LinkedList<String>();
+    private final Semaphore producer_sem = new Semaphore(1, true);
+    private final Semaphore consumer_sem = new Semaphore(1, true);
+    boolean response = false;
+
+    public boolean store (final String n, final String name) {
+        try {
+            producer_sem.acquire();
+
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        if (response) {
+            producer_sem.release();
+            return  response;
+        }
+        if (stock.size() <= 8) {
+            stock.add(n);
+            producer_sem.release();
+        } else if (stock.size() == 9) {
+            response = true;
+            System.out.println("Stock is full");
+            producer_sem.release();
+        } else {
+            System.out.println("Error");
+            producer_sem.release();
+        }
+        return response;
     }
-    
-    public void get() {
-        
+
+    public String load (final String name) {
+        String character = null;
+        try {
+            consumer_sem.acquire();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        if ((response)) {
+            if (stock.isEmpty()){
+                response=false;
+                System.out.println("Store is empty");
+                consumer_sem.release();
+                return character;
+            }
+            character = stock.getFirst();
+            stock.removeFirst();
+        }
+        consumer_sem.release();
+        return character;
     }
-    
-    public void put(ReplaceMe something) {
-        // меняете ReplaceMe на то, что будете помещать внутрь
-    }
-    
 }
