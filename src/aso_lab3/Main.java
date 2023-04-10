c5c9c4a6c39b668b9fed0ef282b25b6bf2ecfd95
Qrroll <dainsureivu@gmail.com>
2023-04-10 22:14:51 +0300

    edited/Main

--- src/aso_lab3/Main.java
+++ src/aso_lab3/Main.java
@@ -6,9 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Store store = new Store();
-        CyclicBarrier barrier = new CyclicBarrier(0); // вместо нуля указываете общее кол-во производителей и потребителей
-        
-        
+        CyclicBarrier barrier = new CyclicBarrier(6); // вместо нуля указываете общее кол-во производителей и потребителей
+
+        Producer p1 = new Producer(store, barrier, "Producer 1");
+        Producer p2 = new Producer(store, barrier, "Producer 2");
+
+        Consumer c1 = new Consumer(store, barrier, "Consumer 1");
+        Consumer c2 = new Consumer(store, barrier, "Consumer 2");
+        Consumer c3 = new Consumer(store, barrier, "Consumer 3");
+        Consumer c4 = new Consumer(store, barrier, "Consumer 4");
+
+        p1.start();
+        p2.start();
+
+        c1.start();
+        c2.start();
+        c3.start();
+        c4.start();
+
     }
     
 }
