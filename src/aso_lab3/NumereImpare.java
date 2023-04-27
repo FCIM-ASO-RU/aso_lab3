8483cc92fca553c9421f1400c1a1b9d09eb3cf1e
Qrroll <dainsureivu@gmail.com>
2023-04-27 21:19:34 +0300

    complete/NumereImpare

--- src/aso_lab3/NumereImpare.java
+++ src/aso_lab3/NumereImpare.java
@@ -1,5 +1,33 @@
 package aso_lab3;
 
-public class NumereImpare {
-    
+import java.util.Iterator;
+import java.util.LinkedList;
+
+class NumereImpare {
+    private int number;
+    public NumereImpare() {
+        number = 0;
+    }
+    public void setNumber(int _number) {
+        number = _number;
+    }
+    public int getNumber() {
+        return number;
+    }
+}
+class NumereImpareStr {
+    public synchronized String getProductsStr(LinkedList<NumereImpare> _products) {
+        String all = "";
+        Iterator<NumereImpare> iterator = _products.iterator();
+        String number;
+        NumereImpare element;
+        while (iterator.hasNext()) {
+            element = iterator.next();
+            number = String.valueOf(element.getNumber());
+            if (number.length() < 2) all = all + " " + number;
+            else all = all + number;
+            if (iterator.hasNext()) all = all + " | ";
+        }
+        return all;
+    }
 }
