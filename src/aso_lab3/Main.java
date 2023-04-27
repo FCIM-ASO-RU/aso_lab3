c220c750f9de2ad3cc48392738bec5d33b6e6b09
Qrroll <dainsureivu@gmail.com>
2023-04-27 21:16:25 +0300

    complete/Main

--- src/aso_lab3/Main.java
+++ src/aso_lab3/Main.java
@@ -1,20 +1,24 @@
 package aso_lab3;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Store store = new Store();
-        CyclicBarrier barrier = new CyclicBarrier(6); // вместо нуля указываете общее кол-во производителей и потребителей
 
-        Producer p1 = new Producer(store, barrier, "Producer 1");
-        Producer p2 = new Producer(store, barrier, "Producer 2");
+        CyclicBarrier barrier = new CyclicBarrier(6);
+        Semaphore semaphore = new Semaphore(1, true);
 
-        Consumer c1 = new Consumer(store, barrier, "Consumer 1");
-        Consumer c2 = new Consumer(store, barrier, "Consumer 2");
-        Consumer c3 = new Consumer(store, barrier, "Consumer 3");
-        Consumer c4 = new Consumer(store, barrier, "Consumer 4");
+        Producer p1 = new Producer(store, semaphore, barrier, "Producer 1");
+        Producer p2 = new Producer(store, semaphore, barrier, "Producer 2");
+
+        Consumer c1 = new Consumer(store, semaphore, barrier, "Consumer 1");
+        Consumer c2 = new Consumer(store, semaphore, barrier, "Consumer 2");
+        Consumer c3 = new Consumer(store, semaphore, barrier, "Consumer 3");
+        Consumer c4 = new Consumer(store, semaphore, barrier, "Consumer 4");
 
         p1.start();
         p2.start();
