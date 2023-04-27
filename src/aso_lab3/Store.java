9c098d7e4447834f97b1b22eae70612da5962cb8
Qrroll <dainsureivu@gmail.com>
2023-04-27 21:19:57 +0300

    complete/Store

--- src/aso_lab3/Store.java
+++ src/aso_lab3/Store.java
@@ -1,27 +1,55 @@
 package aso_lab3;
 
-import java.util.concurrent.Semaphore;
+import java.util.LinkedList;
 
 public class Store {
     
-    private static final int capacity = 0; // указываете в соответствии с вариантом
-    private boolean available; 
-    private final NumereImpare stock; // меняете ReplaceMe на желаемый вами класс-коллекцию
-    
-    final Semaphore producers = new Semaphore(1, true);
-    final Semaphore consumers = new Semaphore(1, true);
-    
+    private static final int capacity = 5; // обьем склада
+    private static final int Z = 50; // обьем продуктов которые нужно произвести и потребить
+    private final LinkedList<NumereImpare> storage;
+
+    private int quantity; // количество обьектов на складе
+    private int produced; // количество произведенных обьектов
+    private int consumed; // количество потребленных обьектов
     public Store() {
-        available = false;
-        stock = new NumereImpare();
+        storage = new LinkedList<>();
+        quantity = 0;
+        produced = 0;
+        consumed = 0;
     }
     
-    public void get() {
-        
+    public synchronized NumereImpare get() {
+        NumereImpare to_get = null;
+        if (quantity - 1 >= 0 && consumed < Z) {
+            quantity -= 1;
+            consumed++;
+            to_get = storage.removeFirst();
+            printPutGet();
+        } else {
+            System.out.println("Store is empty");
+        }
+        return to_get;
     }
     
-    public void put(NumereImpare something) {
-        // меняете ReplaceMe на то, что будете помещать внутрь
+    public synchronized boolean put(NumereImpare something) {
+        if( quantity + 1 <= capacity && produced < Z) {
+            quantity+=1;
+            produced++;
+            storage.add(something);
+            printPutGet();
+            return true;
+        } else {
+            System.out.println("Store is overflow");
+            return false;
+        }
     }
-    
+    synchronized void printPutGet() {
+        System.out.printf("Storage count: %2d | produced total count: %2d | consumed total count: %2d |  numbers: %s \n", quantity, produced, consumed, getProductsStr(storage));
+    }
+    public synchronized boolean getBoolProducerStop() { return produced == Z; }
+    public synchronized boolean getBoolConsumerStop() { return consumed == Z; }
+    public synchronized String getProductsStr(LinkedList<NumereImpare> _products) {
+        NumereImpareStr numstore = new NumereImpareStr();
+        return numstore.getProductsStr(_products);
+    }
 }
