dbbaa231efea0bf1338649733f85ee5e42d57a3b
valio <vali.01@list.ru>
2023-04-30 22:44:11 +0300

    1

--- src/aso_lab3/Store.java
+++ src/aso_lab3/Store.java
@@ -3,25 +3,33 @@ package aso_lab3;
 import java.util.concurrent.Semaphore;
 
 public class Store {
-    
-    private static final int capacity = 0; // указываете в соответствии с вариантом
-    private boolean available; 
-    private final ReplaceMe stock; // меняете ReplaceMe на желаемый вами класс-коллекцию
+    private boolean available;
+    private char number = ' ';
     
     final Semaphore producers = new Semaphore(1, true);
     final Semaphore consumers = new Semaphore(1, true);
-    
-    public Store() {
+
+
+    public synchronized char get() {
+        while (!available) {
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+        }
         available = false;
-        stock = new ReplaceMe();
+        notifyAll();
+        return number;
     }
-    
-    public void get() {
-        
-    }
-    
-    public void put(ReplaceMe something) {
-        // меняете ReplaceMe на то, что будете помещать внутрь
+
+    public synchronized void put(char c) {
+        while (available) {
+            try {
+                wait();
+            } catch (InterruptedException e) { }
+        }
+        this.number = c;
+        available = true;
+        notifyAll();
     }
     
 }
