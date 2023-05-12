8121a79088836ab8f851007e16717775d0a963ef
Vadim Shvets <vadimshvets@gmail.com>
2023-05-12 16:26:38 +0300

    added producer and main

--- src/aso_lab3/Main.java
+++ src/aso_lab3/Main.java
@@ -5,10 +5,21 @@ import java.util.concurrent.CyclicBarrier;
 public class Main {
 
     public static void main(String[] args) {
-        Store store = new Store();
-        CyclicBarrier barrier = new CyclicBarrier(0); // вместо нуля указываете общее кол-во производителей и потребителей
-        
-        
+        final Store store = new Store();
+        final CyclicBarrier barrier = new CyclicBarrier(6);
+        final Producer p1 = new Producer(store, barrier, "Producer 1");
+        final Producer p2 = new Producer(store, barrier, "Producer 2");
+        final Producer p3 = new Producer(store, barrier, "Producer 3");
+        final Consumer c1 = new Consumer(store, barrier, "Consumer 1");
+        final Consumer c2 = new Consumer(store, barrier, "Consumer 2");
+        final Consumer c3 = new Consumer(store, barrier, "Consumer 3");
+        p1.start();
+        p2.start();
+        p3.start();
+        c1.start();
+        c2.start();
+        c3.start();
+
     }
-    
+
 }
--- src/aso_lab3/Producer.java
+++ src/aso_lab3/Producer.java
@@ -1,24 +1,41 @@
 package aso_lab3;
 
+import java.util.Objects;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class Producer extends Thread {
-    
-    private static final int max = 0; // указываете в соответствии с вариантом
-    private static final ReplaceMe[] products = new ReplaceMe[]{}; // меняете на то, что должно производится
-    
     private final Store store;
+
+    private int number;
+
+    private char[] array;
     private final CyclicBarrier barrier;
-    
+
     public Producer(Store _store, CyclicBarrier _barrier, String name) {
         store = _store;
         barrier = _barrier;
         setName(name);
+        array = new char[]{'a','e','i','u','o'};
     }
-    
+
     @Override
     public void run() {
-        
+        if(Objects.isNull(array)){
+            try {
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        else
+            for (int i = 0; i <2; i++) {
+                store.put(array[i]);
+                System.out.println(this.getName() + " put: " + i);
+                try {
+                    sleep((int)(Math.random() * 100));
+                } catch (InterruptedException e) { }
+            }
     }
-    
+
 }
