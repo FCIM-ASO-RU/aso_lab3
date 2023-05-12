86443a75313757b06120d9814725587bee948088
Vadim Shvets <vadimshvets@gmail.com>
2023-05-12 16:19:23 +0300

    added consumator

--- src/aso_lab3/Consumer.java
+++ src/aso_lab3/Consumer.java
@@ -1,23 +1,36 @@
 package aso_lab3;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class Consumer extends Thread {
-    
-    private static final int max = 0; // указываете в соответствии с вариантом
-    
     private final Store store;
+
+    private int number;
     private final CyclicBarrier barrier;
-    
+
     public Consumer(Store _store, CyclicBarrier _barrier, String name) {
         store = _store;
         barrier = _barrier;
         setName(name);
     }
-    
-    @Override
+
     public void run() {
-        
+        char value = 0;
+        for (int i = 0; i < 2; i++) {
+            value = store.get();
+            if(value == 0){
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+            else {
+                System.out.println(this.getName() + " got: " + value);
+            }
+        }
     }
-    
+
 }
